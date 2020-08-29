2122d4cb30d0e7bff82c955a76a530e9a30cb645
sunkusaipurnima <69891257+sunkusaipurnima@users.noreply.github.com>
2020-08-29 14:26:52 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -15,7 +15,12 @@ function draw() {
   movingRect.x = World.mouseX;
   movingRect.y = World.mouseY;
 
-  if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2
+  isTouching();
+  drawSprites();
+}
+function isTouching(){
+  
+ if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2
       && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2
       && movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2
       && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2) {
@@ -26,5 +31,8 @@ function draw() {
     movingRect.shapeColor = "green";
     fixedRect.shapeColor = "green";
   }
-  drawSprites();
 }
+  
+  
+  
+      
